6c25cdd98df05caea708bb1d15600ddbb0c9d8f9
Isa199510 <isa.arbuhov@mail.ru>
2022-10-29 23:18:07 +0600

    setup HW2

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redTrafficLights: UIView!
+    @IBOutlet var yellowTrafficLights: UIView!
+    @IBOutlet var greenTrafficLights: UIView!
+    
+    var traffictLight: TraffictLamp = .red
+    var ligthIsOn: CGFloat = 1
+    var lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redTrafficLights.alpha = lightIsOff
+        yellowTrafficLights.alpha = lightIsOff
+        greenTrafficLights.alpha = lightIsOff
+        
     }
-
-
+    
+    override func viewWillLayoutSubviews() {
+        redTrafficLights.layer.cornerRadius = redTrafficLights.frame.height / 2
+        yellowTrafficLights.layer.cornerRadius = yellowTrafficLights.frame.height / 2
+        greenTrafficLights.layer.cornerRadius = greenTrafficLights.frame.height / 2
+    }
+    
+    @IBAction func nextTrafficLightButton(_ sender: UIButton) {
+        if sender.titleLabel?.text == "START" {
+            sender.setTitle("NEXT", for: .normal)
+        }
+        
+        switch traffictLight {
+        case .red:
+            redTrafficLights.alpha = ligthIsOn
+            greenTrafficLights.alpha = lightIsOff
+            traffictLight = .yellow
+        case .yellow:
+            yellowTrafficLights.alpha = ligthIsOn
+            redTrafficLights.alpha = lightIsOff
+            traffictLight = .green
+        case .green:
+            greenTrafficLights.alpha = ligthIsOn
+            yellowTrafficLights.alpha = lightIsOff
+            traffictLight = .red
+        }
+    }
+    
 }
 
+enum TraffictLamp {
+    case red
+    case yellow
+    case green
+}
